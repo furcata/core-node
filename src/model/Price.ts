71dae4531285fbc99e4258d8f37e074b29376801
Erny Sans <ernysans@icloud.com>
2025-03-25 21:35:35 -0500

    feat: add Price namespace with Type enum and interface for pricing structure

---
+++ src/model/Price.ts
@@ -0,0 +1,26 @@
+import {BaseFirestore} from '../interface/index.js';
+
+/**
+ * @license
+ * Copyright Furcata. All Rights Reserved.
+ * Uses stripe price structure
+ */
+export namespace Price {
+  export enum Type {
+    oneTime = 'one_time',
+    recurring = 'recurring',
+  }
+
+  export interface Interface extends BaseFirestore {
+    active?: boolean;
+    amount?: number;
+    currency?: string;
+    event?: string;
+    label?: string;
+    description?: string;
+    // Limit the number of users that can pay for this price
+    limit?: number;
+    type?: Type;
+    users?: string[]; // user ids that have paid for this price
+  }
+}
--- src/model/index.ts
+++ src/model/index.ts
@@ -7,3 +7,4 @@ export * from './Block.js';
 export * from './EventData.js';
 export * from './MessagingEvent.js';
 export * from './Post.js';
+export * from './Price.js';
