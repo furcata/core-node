2ff313a3bea8ab12384e3a8bc64d20fd21628498
Erny Sans <ernysans@icloud.com>
2025-03-25 21:11:37 -0500

    chore: add postinstall script to run compile after installation

--- lib/model/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright Furcata. All Rights Reserved.
- */
-export * from './Account.js';
-export * from './Block.js';
-export * from './EventData.js';
-export * from './MessagingEvent.js';
-export * from './Post.js';
